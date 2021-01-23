a8127dccb9437f2bb3a2966b8a94e764d5ae0095
Patrick Zanzarini <patrick.silva@hbsis.com.br>
2021-01-23 13:36:32 -0300

    Cria form de login

---
+++ RestaurantePizzariaUi/src/app/modules/login/components/login-form/login-form.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { validateFormFields } from 'src/app/core/utils/form-helper';
+
+@Component({
+  selector: 'rp-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.less'],
+})
+export class LoginFormComponent implements OnInit {
+  @Output() entrar = new EventEmitter<{ usuario: string; senha: string }>();
+
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      usuario: [{ value: '', disabled: false }, [Validators.required]],
+      senha: [{ value: '', disabled: false }, [Validators.required]],
+    });
+  }
+
+  submitLogin() {
+    validateFormFields(this.loginForm);
+    
+    if (this.loginForm.valid) {
+      var dados = {
+        usuario: this.loginForm.get('nome').value,
+        senha: this.loginForm.get('senha').value,
+      }
+      this.entrar.emit(dados);
+    }
+  }
+}
--- RestaurantePizzariaUi/src/app/modules/login/login.module.ts
+++ RestaurantePizzariaUi/src/app/modules/login/login.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginPage } from './pages/login/login.page';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginPage, LoginFormComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatInputModule,
+    LoginRoutingModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
-export class LoginModule { }
+export class LoginModule {}
