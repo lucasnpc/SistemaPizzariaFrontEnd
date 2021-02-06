8b05a05b3ea3ecca786f9328c31de65439ab938a
lucasnpc <lucasnpc2@outlook.com>
2021-02-06 13:40:03 -0300

    Adicao do grafico

--- RestaurantePizzariaUi/src/app/modules/contas/components/grafico-contas/grafico-contas.component.ts
+++ RestaurantePizzariaUi/src/app/modules/contas/components/grafico-contas/grafico-contas.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+export var single = [
+  {
+    "name": "Recebidos",
+    "value": 55
+  },
+  {
+    "name": "Pagos",
+    "value": 55
+  },
+];
+
 @Component({
   selector: 'rp-grafico-contas',
   templateUrl: './grafico-contas.component.html',
@@ -7,9 +18,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraficoContasComponent implements OnInit {
 
-  constructor() { }
+  single: any[];
+  view: any = [600, 193.99];
+
+  // options
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showLabels: boolean = true;
+  isDoughnut: boolean = false;
+
+  colorScheme = {
+    domain: ['#5AA454', '#dbd945']
+  };
+
+  constructor() { Object.assign(this, { single }); }
 
   ngOnInit(): void {
+  }
+
+  onSelect(data: any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data: any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data: any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
   }
 
 }
