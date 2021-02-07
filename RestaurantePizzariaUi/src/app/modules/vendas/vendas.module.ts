6643352c673520da53d3aee9ba7f3a3bf1ebee4e
lucasnpc <lucasnpc2@outlook.com>
2021-02-06 22:08:42 -0300

    Adequacao do layout as regras de negocio

--- RestaurantePizzariaUi/src/app/modules/vendas/vendas.module.ts
+++ RestaurantePizzariaUi/src/app/modules/vendas/vendas.module.ts
@@ -6,21 +6,22 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { ListaPedidosComponent } from './components/lista-pedidos/lista-pedidos.component';
 import { ListaComandasComponent } from './components/lista-comandas/lista-comandas.component';
 import { ListaVendasComponent } from './components/lista-vendas/lista-vendas.component';
-import { NotificacaoWppComponent } from './components/notificacao-wpp/notificacao-wpp.component';
 import { ValorMesComponent } from './components/valor-mes/valor-mes.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import {MatButtonModule} from '@angular/material/button';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @NgModule({
-  declarations: [VendasPage, ListaPedidosComponent, ListaComandasComponent, ListaVendasComponent, NotificacaoWppComponent, ValorMesComponent],
+  declarations: [VendasPage, ListaPedidosComponent, ListaComandasComponent, ListaVendasComponent, ValorMesComponent],
   imports: [
     CommonModule,
     VendasRoutingModule,
     MatGridListModule,
     MatCardModule,
     MatTableModule,
-    MatButtonModule
+    MatButtonModule,
+    MatExpansionModule
   ],
   providers: []
 })
