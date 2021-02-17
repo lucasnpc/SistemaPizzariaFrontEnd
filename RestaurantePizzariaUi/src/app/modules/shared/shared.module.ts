db3e9c04bd02e38e9bf0e2b4a71b531dd399056c
lucasnpc <lucasnpc2@outlook.com>
2021-02-17 16:38:29 -0300

    Add side nav do angular material

--- RestaurantePizzariaUi/src/app/modules/shared/shared.module.ts
+++ RestaurantePizzariaUi/src/app/modules/shared/shared.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutMenuComponent } from './components/layout-menu/layout-menu.component';
 import { RouterModule } from '@angular/router';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
-  declarations: [LayoutMenuComponent, SidebarComponent],
-  imports: [CommonModule, RouterModule, MatButtonModule, MatIconModule, MatToolbarModule],
+  declarations: [LayoutMenuComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
+  ],
   exports: [LayoutMenuComponent],
 })
 export class SharedModule {}
