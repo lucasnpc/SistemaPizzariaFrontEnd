1e0ac05538380ed2fa66689b42fe046a5e193bed
lucasnpc <lucasnpc2@outlook.com>
2021-11-20 20:38:26 -0300

    Finishing dashboard layout

--- RestaurantePizzariaUi/src/app/modules/dashboard/components/gains/gains.component.ts
+++ RestaurantePizzariaUi/src/app/modules/dashboard/components/gains/gains.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'rp-gains',
   templateUrl: './gains.component.html',
-  styleUrls: ['./gains.component.less']
+  styleUrls: ['./gains.component.less'],
 })
 export class GainsComponent implements OnInit {
+  gains = 50;
+  expenses = 50;
+  total = this.gains - this.expenses;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
